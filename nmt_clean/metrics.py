625579c8d3efbda90a09635c59f8a7273411d420
mekaneeky <azawahry@sunbird.ai>
2023-03-01 18:41:07 +0300

    added eval_lang argument for specifying specific evaluation pairs in many2many

--- nmt_clean/metrics.py
+++ nmt_clean/metrics.py
@@ -6,7 +6,7 @@ def postprocess(preds, labels):
     return preds, labels
 
 
-def compute_sacreBLEU(eval_preds, eval_languages, samples_per_language, tokenizer, metric):
+def compute_sacreBLEU(eval_preds, eval_languages, samples_per_language, tokenizer, metric, eval_lang = None):
     preds, labels = eval_preds
     if isinstance(preds, tuple):
         preds = preds[0]
@@ -22,6 +22,8 @@ def compute_sacreBLEU(eval_preds, eval_languages, samples_per_language, tokenize
     
     result = {}
     for i, lang in enumerate(eval_languages):
+        if eval_lang is not None and eval_lang not in lang:
+            continue
         result_subset = metric.compute(
             predictions=decoded_preds[i*samples_per_language:(i+1)*samples_per_language],
             references=decoded_labels[i*samples_per_language:(i+1)*samples_per_language])
@@ -31,3 +33,5 @@ def compute_sacreBLEU(eval_preds, eval_languages, samples_per_language, tokenize
     
     result = {k: round(v, 4) for k, v in result.items()}
     return result
+
+
